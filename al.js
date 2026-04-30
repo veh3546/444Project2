72cd44bca2b58b7d7fa6c1c6643d698201434def
veh3546 <veh3546@g.rit.edu>
2026-04-30 09:50:43 -0400

    finished application layer framework

--- al.js
+++ al.js
@@ -212,8 +212,16 @@ app.get('/login', (req, res) => {
  * The post method to insert a new book into the DB
  * 
  * EX. http://localhost:3000/add
+ *  Send JSON body
+ * {
+        "title" : "The Odyssey",
+        "author" : "Homer",
+        "description" : "This sample description might be 2 sentances",
+        "genre" : "Epic",
+        "year_published" : 800,
+        "publisher" : "Greeks"
+    }
  * 
- * @param genre - the genre of the book
  */
 app.post('/add', (req, res) => {
 
@@ -227,13 +235,129 @@ app.post('/add', (req, res) => {
     //const bookID = db.insertBook(title, author, description, genre, year_published, publisher);
 
     //on success
-    // res.status(200).json({ genreBooks });
+    // res.status(200).json({ bookID });
+
+    // on failure
+    // res.status(400).json({"error": "Invalid Params"});
+});
+
+// ************************* PUT **********************
+
+
+/**
+ * The put method to update a book with new params
+ * 
+ * EX. http://localhost:3000/updateBook
+ * sent JSON
+  * {
+  *     "bookID" : 1,
+        "title" : "The Odyssey",
+        "author" : "Homer",
+        "description" : "This sample description might be 2 sentances",
+        "genre" : "Epic",
+        "year_published" : 800,
+        "publisher" : "Greeks"
+    }
+ * 
+ */
+app.put('/updateBook', (req, res) => {
+
+    // Gets the params
+    const { bookID, title, author, description, genre, year_published, publisher } = req.body
+    // console.log(userID);
+
+    //TODO: check if valid data Types
+
+    //TODO: check if valid bookID
+    
+    //if all valid data types, attempt to update book with new book object
+    //const bookID = db.updateBook(bookID, title, author, description, genre, year_published, publisher);
+
+    //on success
+    // res.status(200).json({ bookID });
 
     // on failure
     // res.status(400).json({"error": "Invalid Params"});
 });
 
 
+/**
+ * The post method to insert a new book into the DB
+ * 
+ * EX. http://localhost:3000/loanBook
+ * JSON
+ * {
+ *  'bookID' : 1,
+ *  'userID' : 1,
+ *  'dateParam' : '2026-05-15'
+ * }
+ */
+app.put('/loanBook', (req, res) => {
+
+    // Gets the params
+ const { bookID, userID, dateParam } = req.body
+    // console.log(booID, dateParam);
+
+    //TODO: check if valid data Types
+
+    //TODO: check if valid userID
+
+    //TODO: check if valid bookID
+    //const is_loaned = db.validBook(bookID) ( )
+    const is_loaned; //TEMP
+    
+    //If invalid book
+    if(is_loaned == null){
+        res.status(400).json({"error": "Invalid Params"});
+    }
+
+    //if all valid data types and loan is false, update is_loaned status and create new loan objectx
+    if (is_loaned == false){
+         //db.updateBookLoan(bookID, true);
+        //db.insertLoan(bookID, userID, dateParam);
+
+        // on success
+        res.status(200).json({ 'message': 'Book succesffuly loaned '});
+    }
+    else{
+        //db.updateBookLoan(bookID, false);
+        //db.updateLoan(bookID, userID, dateParam);
+
+           
+        //on success
+        res.status(200).json({ 'message': 'Book succesffuly returned '});
+    }
+
+
+});
+
+// ************************* DELETE ***********************************8
+
+/**
+ * The delete method for getting rid of a book
+ * 
+ * EX. http://localhost:3000/loanBook?bookID='1'
+ * 
+ * @param bookID = the books id
+ */
+app.get('/book', (req, res) => {
+    // Gets the id or -1 if invalid
+    const bookID = req.query.bookID != undefined ? req.query.bookID: -1;
+    // console.log(bookID);
+
+    //TODO: check if valid data Type
+
+    //TODO: validBook
+    
+    //if valid data type, attempt delete book
+    //db.deleteBook(bookID)
+
+    //on success
+    res.status(200).json({ 'message': 'Book successfully deleted' });
+
+    // on failure
+    res.status(400).json({"error": "Invalid Params"});
+});
 
 
 // Start the server
