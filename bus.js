acccc3ab89bb75500c71ea3f63ec10365be099a1
barbelo9885 <bda9885@rit.edu>
2026-05-03 11:15:38 -0400

    Added validation functions

--- bus.js
+++ bus.js
@@ -1,14 +1,23 @@
 
 function isNum(value){
+    if(typeof value == "number") {
+        return Number.isInteger(value);
+    } else if(typeof value == "string") {
+        // if inputted as a string, test if the string only contains digits
+        let pattern = /\D/g;
+        return !pattern.test(value);
+    }
     return true;
 }
 
 function isString(value){
-    return true;
+    return (typeof value == "string");
 }
 
 function isValidDate(value){
-    return true;
+    // uses format YYYY-MM-DD
+    let pattern = /\d\d\d\d-\d\d-\d\d/;
+    return (pattern.test(value) && value.length == 10);
 }
 
 function hash(value){
